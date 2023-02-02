fae0145349c46b35538cfc9e3bf0abcc4f7f524a
Pablo <pablolega7@gmail.com>
2023-02-02 18:13:24 +0100

    Creation LoginPage

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import 'animate.css'
+import { CalendarApp } from './CalendarApp'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+ <CalendarApp/>
+  </React.StrictMode>
+)
